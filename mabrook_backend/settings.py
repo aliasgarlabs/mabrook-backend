ea569fd3f31181b62d9936a16a6a018a677197a8
Mufaddal Patanwala <mufaddal1125@gmail.com>
2021-05-18 10:47:30 +0400

    remove permissions

--- mabrook_backend/settings.py
+++ mabrook_backend/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissions'
+        # 'rest_framework.permissions.DjangoModelPermissions'
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
