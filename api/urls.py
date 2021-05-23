5138523e4c17e0824e339f2d82c7192e0ecf02ca
Aliasgar Murtaza <aliasgarlabs@gmail.com>
2021-05-23 23:14:33 +0400

    Add viewset

--- api/urls.py
+++ api/urls.py
@@ -8,7 +8,7 @@ router.register(r'', views.EventViewSet)
 router.register(r'', views.FireAndIceViewSet)
 
 urlpatterns = [
-    path('fireandice', include(router.urls), name='get_fire_or_ice'),
+    path('fireandice', views.get_fire_or_ice, name='get_fire_or_ice'),
     path('events', include(router.urls), name='get_events'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api-get-token/', obtain_auth_token, name='gettoken'),
